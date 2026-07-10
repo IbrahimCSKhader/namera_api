5e49573f8b3fa706582f05b866c17cc73dfaa269
IbrahimCSKhader <ik2907951@gmail.com>
2026-07-10 16:30:20 +0300

    Allow frontend CORS

--- Extensions/ServiceCollection/CorsServiceExtensions.cs
+++ Extensions/ServiceCollection/CorsServiceExtensions.cs
@@ -4,15 +4,12 @@ public static class CorsServiceExtensions
 {
     public static IServiceCollection AddCorsServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>()
-            ?? ["http://localhost:5173", "http://127.0.0.1:5173"];
-
         services.AddCors(options =>
         {
             options.AddPolicy("ReactFrontend", policy =>
             {
                 policy
-                    .WithOrigins(allowedOrigins)
+                    .AllowAnyOrigin()
                     .AllowAnyHeader()
                     .AllowAnyMethod();
             });
