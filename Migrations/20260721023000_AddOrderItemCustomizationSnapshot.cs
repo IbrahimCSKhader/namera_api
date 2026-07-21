65a855be82805dab4562f4f0549ee3df42e01bf7
IbrahimCSKhader <ik2907951@gmail.com>
2026-07-21 22:55:32 +0300

    Register order customization migration

--- Migrations/20260721023000_AddOrderItemCustomizationSnapshot.cs
+++ Migrations/20260721023000_AddOrderItemCustomizationSnapshot.cs
@@ -8,30 +8,36 @@ namespace namera_API.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "CustomizationDetailsJson",
-                table: "OrderItems",
-                type: "nvarchar(4000)",
-                maxLength: 4000,
-                nullable: true);
+            migrationBuilder.Sql("""
+                IF COL_LENGTH(N'dbo.OrderItems', N'CustomizationDetailsJson') IS NULL
+                BEGIN
+                    ALTER TABLE [OrderItems] ADD [CustomizationDetailsJson] nvarchar(4000) NULL;
+                END
+                """);
 
-            migrationBuilder.AddColumn<string>(
-                name: "CustomizationSummary",
-                table: "OrderItems",
-                type: "nvarchar(1200)",
-                maxLength: 1200,
-                nullable: true);
+            migrationBuilder.Sql("""
+                IF COL_LENGTH(N'dbo.OrderItems', N'CustomizationSummary') IS NULL
+                BEGIN
+                    ALTER TABLE [OrderItems] ADD [CustomizationSummary] nvarchar(1200) NULL;
+                END
+                """);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "CustomizationDetailsJson",
-                table: "OrderItems");
+            migrationBuilder.Sql("""
+                IF COL_LENGTH(N'dbo.OrderItems', N'CustomizationDetailsJson') IS NOT NULL
+                BEGIN
+                    ALTER TABLE [OrderItems] DROP COLUMN [CustomizationDetailsJson];
+                END
+                """);
 
-            migrationBuilder.DropColumn(
-                name: "CustomizationSummary",
-                table: "OrderItems");
+            migrationBuilder.Sql("""
+                IF COL_LENGTH(N'dbo.OrderItems', N'CustomizationSummary') IS NOT NULL
+                BEGIN
+                    ALTER TABLE [OrderItems] DROP COLUMN [CustomizationSummary];
+                END
+                """);
         }
     }
 }
