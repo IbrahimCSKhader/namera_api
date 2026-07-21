7eb25b6eceb331e547cac559a090752c02852778
IbrahimCSKhader <ik2907951@gmail.com>
2026-07-21 22:42:53 +0300

    Configure render frontend CORS origin

--- Extensions/ServiceCollection/CorsServiceExtensions.cs
+++ Extensions/ServiceCollection/CorsServiceExtensions.cs
@@ -4,17 +4,36 @@ public static class CorsServiceExtensions
 {
     public static IServiceCollection AddCorsServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>()
+            ?? DefaultAllowedOrigins;
+
         services.AddCors(options =>
         {
             options.AddPolicy("ReactFrontend", policy =>
             {
-                policy
-                    .AllowAnyOrigin()
-                    .AllowAnyHeader()
+                if (allowedOrigins.Contains("*", StringComparer.Ordinal))
+                {
+                    policy.AllowAnyOrigin();
+                }
+                else
+                {
+                    policy.WithOrigins(allowedOrigins);
+                }
+
+                policy.AllowAnyHeader()
                     .AllowAnyMethod();
             });
         });
 
         return services;
     }
+
+    private static readonly string[] DefaultAllowedOrigins =
+    [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://namera-front.onrender.com",
+        "https://resinbonn.runasp.net",
+        "http://resinbonn.runasp.net"
+    ];
 }
